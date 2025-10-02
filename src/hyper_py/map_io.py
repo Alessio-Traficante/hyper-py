Corrected a small bug when header units are "MJy/sr"

src/hyper_py/map_io.py
@@ -53,7 +53,7 @@ def read_and_prepare_map(filepath, beam, beam_area_arcsec2, beam_area_sr, conver
     header_for_units = hdul[0].header
     bunit = header_for_units.cards['BUNIT'].value
 
-    if bunit == 'MJy /sr':
+    if bunit == 'MJy/sr' or bunit == 'MJy / sr':
         arcsec_to_rad = np.pi / (180.0 * 3600.0)
         pix_area_sr = (pix_dim * arcsec_to_rad)**2
         image_data *= 1e6 * pix_area_sr  # MJy/sr to Jy/pixel